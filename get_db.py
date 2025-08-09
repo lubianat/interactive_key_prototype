1025c16113665eb014a17ee385a4911e47c355ac
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2025-08-09 10:16:58 -0300

    feat: pull identifiers from Wikidata
    closes #1

--- get_db.py
+++ get_db.py
@@ -16,7 +16,7 @@ PREFIX pq: <https://reflora-traits-test.wikibase.cloud/prop/qualifier/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX schema: <http://schema.org/>
 
-SELECT ?item ?itemLabel ?prop ?propLabel ?propDirect ?descriptor ?descriptorLabel ?value ?valueLabel ?image
+SELECT ?item ?itemLabel ?prop ?propLabel ?propDirect ?descriptor ?descriptorLabel ?value ?valueLabel ?image ?wikidataID
 WHERE {
   wd:Q13 wdt:P6 ?item .
 
@@ -30,6 +30,8 @@ WHERE {
   ?statement pq:P4 ?value .
 
   OPTIONAL { ?item wdt:P11 ?image }
+
+  OPTIONAL { ?item wdt:P12 ?wikidataID }
 
   ?item rdfs:label ?itemLabel .
   FILTER(LANG(?itemLabel) = "pt")
@@ -72,11 +74,13 @@ def format_results(bindings: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
 
         # Optional image filename literal from SPARQL
         image_url = row.get("image", {}).get("value")
+        wikidata_id = row.get("wikidataID", {}).get("value")
 
         if item_qid not in items:
             items[item_qid] = {
                 "name": item_label,
                 "wikibase": item_qid,
+                "wikidata_xref": wikidata_id,
                 "traits": defaultdict(dict),
                 # store both raw filename and a ready-to-use Commons URL (if present)
                 **({"imageFilename": image_url} if image_url else {}),
@@ -95,6 +99,7 @@ def format_results(bindings: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
         base = {
             "name": obj["name"],
             "wikibase": obj["wikibase"],
+            "wikidata_xref": obj["wikidata_xref"],
             "traits": traits_clean,
         }
         if "imageURL" in obj:
